39e3e515b1fc1478d6d76ce936ec9fb55cbc94dd
qwe321qwe321qwe321 <aegis321123@gmail.com>
2024-03-19 17:34:51 +0800

    Extract SwitchSceneHelper class

--- Editor/RecentSceneToolbarGUI.cs
+++ Editor/RecentSceneToolbarGUI.cs
@@ -63,32 +63,4 @@ namespace RecentSceneToolbar {
 			menu.ShowAsContext();
 		}
 	}
-
-	static class SwitchSceneHelper {
-		static string scenePathToOpen;
-
-		public static void StartScene(string scenePath) {
-			if (EditorApplication.isPlaying) {
-				EditorApplication.isPlaying = false;
-			}
-
-			scenePathToOpen = scenePath;
-			EditorApplication.update += OnUpdate;
-		}
-
-		static void OnUpdate() {
-			if (scenePathToOpen == null ||
-				EditorApplication.isPlaying || EditorApplication.isPaused ||
-				EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode) {
-				return;
-			}
-
-			EditorApplication.update -= OnUpdate;
-
-			if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
-				EditorSceneManager.OpenScene(scenePathToOpen);
-			}
-			scenePathToOpen = null;
-		}
-	}
 }
---
+++ Editor/SwitchSceneHelper.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace RecentSceneToolbar {
+    static class SwitchSceneHelper {
+        static string scenePathToOpen;
+
+        public static void StartScene(string scenePath) {
+            if (EditorApplication.isPlaying) {
+                EditorApplication.isPlaying = false;
+            }
+
+            scenePathToOpen = scenePath;
+            EditorApplication.update += OnUpdate;
+        }
+
+        static void OnUpdate() {
+            if (scenePathToOpen == null ||
+                EditorApplication.isPlaying || EditorApplication.isPaused ||
+                EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode) {
+                return;
+            }
+
+            EditorApplication.update -= OnUpdate;
+
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo()) {
+                EditorSceneManager.OpenScene(scenePathToOpen);
+            }
+            scenePathToOpen = null;
+        }
+    }
+}
